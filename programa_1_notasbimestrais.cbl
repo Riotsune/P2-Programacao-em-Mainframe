cf7a5b70681e9228e2a7afaf684505d3ac999d7b
Elder <elder_louzada@outlook.com>
2019-09-29 23:22:12 -0300

    atualiza programa 1

--- programa_1_notasbimestrais.cbl
+++ programa_1_notasbimestrais.cbl
@@ -8,42 +8,77 @@
 
        data division.
        working-storage section.
+       01 Aluno pic A(60) value space.
        01 Notas.
-           02 PrimeiraNota pic 9(2)V99 value zeros.
-           02 SegundaNota pic 9(2)V99 value zeros.
-           02 TerceiraNota pic 9(2)V99 value zeros.
-           02 QuartaNota pic 9(2)V99 value zeros.
+           02 NotaP1 pic 9(2)V99 value zeros.
+           02 NotaP2 pic 9(2)V99 value zeros.
            02 MascaraNota pic zz.zz9,99.
        01 Media pic 9(2)V99 value zeros.
+       01 PressEnter pic A.
 
        procedure division.
            perform mostra-mensagens.
            perform recebe-valores.
            perform calcula-media.
+           display MascaraNota at 1425.
+           accept PressEnter at 2000
+           if Media < 6 then
+              perform mensagem-reprovacao1
+              perform recebe-exame-suplementar
+              perform calcula-media
+              if Media < 6 then
+                 perform mensagem-reprovacao2
+              else
+                 perform mensagem-aprovacao
+              end-if
+           else
+              perform mensagem-aprovacao
+           end-if.
            stop run.
 
        mostra-mensagens.
-           display "----- Digite as suas notas -----" at 0303.
-           display "1o Bimestre: "  at 0608.
-           display "2o Bimestre: "  at 0808.
-           display "3o Bimestre: "  at 1008.
-           display "4o Bimestre: " at 1208.
-           display "Media: " at 1508.
+           display "----- Digite as suas notas -----" at 0308.
+           display "Digite seu nome: "  at 0608.
+           display "P1: "  at 0808.
+           display "P2: "  at 1008.
+           display "Media: "  at 1408.
+
+       mensagem-reprovacao1.
+           display "----- Sua media nao alcancou o valor de 6 -----"
+               with blank screen 
+           at 0308.
+           display "P3: "  at 0608.
+       
+       mensagem-reprovacao2.
+           display "----- REPROVADO -----"
+               with blank screen  at 0308.
+           display "Media: "  at 0608.
+           display MascaraNota at 0625.
+       
+       mensagem-aprovacao.
+           display "----- APROVADO -----"
+               with blank screen  at 0308.
+           display "Media: "  at 0608.
+           display MascaraNota at 0625.
 
        recebe-valores.
-           accept MascaraNota at 0625.
-           move MascaraNota to PrimeiraNota.
+           accept Aluno at 0625.
            accept MascaraNota at 0825.
-           move MascaraNota to SegundaNota.
+           move MascaraNota to NotaP1.
            accept MascaraNota at 1025.
-           move MascaraNota to TerceiraNota.
-           accept MascaraNota at 1225.
-           move MascaraNota to QuartaNota.
+           move MascaraNota to NotaP2.
+        
+       recebe-exame-suplementar.
+           accept MascaraNota at 0625.
+           if NotaP1 < NotaP2 then
+               move MascaraNota to NotaP1
+           else
+               move MascaraNota to NotaP2
+           end-if.
        
        calcula-media.
-           add PrimeiraNota SegundaNota TerceiraNota QuartaNota 
+           add NotaP1 NotaP2 
            to Media.
-           divide Media by 4 giving Media.
+           divide Media by 2 giving Media.
            move Media to MascaraNota.
-           display MascaraNota at 1525.
         
